0b325a58868b1da9d97a6ecb70b46ea313db4af8
Marion Office <marionoffic@gmail.com>
2022-07-24 07:51:56 +0000

    added condition I forgot that activates if player has paper and computer selection is scissors. Also added console.logs for debuggin.Also i just realized i need to account for the player putting in capitals so im going to account for that but i wont use another dommint just for dat. Now im finished tho

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let rpsSelection = ['rock', 'paper', 'scissors'];
-const playerChoice = window.prompt("Rock, paper, or scissors: ");
 function getComputerChoice(rpsSelection){
     let ranNumber = Math.floor(Math.random() * 3);
     return rpsSelection[ranNumber];
@@ -7,21 +6,34 @@ function getComputerChoice(rpsSelection){
 
 let computerChoice = getComputerChoice(rpsSelection);
 
+const playerChoice = window.prompt("Rock, paper, or scissors: ");
+
 function playRps(computerChoice, playerChoice){
-        if (computerChoice == playerChoice){
-            alert('tie!')}
-         else if (computerChoice == 'rock' && playerChoice == 'scissors'){
-            alert('you lose, rock beats scissors..')}
+    console.log('???')
+    if (computerChoice == playerChoice){
+            alert('tie!')
+            console.log(1)}
+        else if (computerChoice == 'rock' && playerChoice == 'scissors'){
+            alert('you lose, rock beats scissors..')
+            console.log(2)}
         else if (computerChoice == 'scissors' && playerChoice == 'rock'){
-            alert('you win! rock beats scissors')}
+            alert('you win! rock beats scissors')
+            console.log(3)}
         else if(computerChoice == 'paper' && playerChoice == 'scissors'){
-            alert('you win! scissors beats paper!')}
+            alert('you win! scissors beats paper!')
+            console.log(4)}
         else if (computerChoice == 'rock' && playerChoice == 'paper'){
-            alert('you win! Paper beats rock')}
+            alert('you win! Paper beats rock')
+            console.log(5)}
         else if (computerChoice == 'paper' && playerChoice == 'rock'){
-            alert('you lose, paper beats rock...')}
-        else (computerChoice == 'rock' && playerChoice == 'scissors'){
-            alert('you lose, rock beats scissors...')};
+            alert('you lose, paper beats rock...')
+            console.log(6)}
+        else if (computerChoice == 'rock' && playerChoice == 'scissors'){
+            alert('you lose, rock beats scissors...')
+            console.log(7)}
+        else if (computerChoice == 'scissors' && playerChoice == 'paper'){
+           alert('you lose, scissors beats rock...')
+            console.log(8)};
 };
 playRps(computerChoice, playerChoice);
 
